ca2a6b4c5cfdf4831601c815f622be95be826bb8
Krishnan. N <scratch@srijan.in>
2013-05-23 15:34:31 +0530

    added ocean sound

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
 var hoverOnFlag = document.getElementById("mainpage_flag");
+var hoverOnOcean = document.getElementById("mainpage_ocean");
 var audioPlayingFlg = 0;
+var audioPlayingID;
 
+
+// TODO: Pause / play should be a fn, called after simply setting ID.
 
 hoverOnFlag.onclick=function()  {   // TODO: As the name implies, work on hover, not click
 	var whitePlay = document.getElementById("white_play");
 	
+	// Bug: If playing a different song and click here, should stop the previous song
+	// and play this song; currently, click is a toggle
     if (audioPlayingFlg == 0) { 
 		whitePlay.play();
 		audioPlayingFlg = 1;
 	} else  {
-		whitePlay.pause();
+		audioPlayingID.pause();
 		audioPlayingFlg = 0;
 	}
+			
+	audioPlayingID = whitePlay;
+
+}
+
+
+hoverOnOcean.onclick=function()  {   // TODO: As the name implies, work on hover, not click
+	var oceanPlay = document.getElementById("ocean_play");
+	
+	// Bug: If playing a different song and click here, should stop the previous song
+	// and play this song; currently, click is a toggle
+    if (audioPlayingFlg == 0) { 
+		oceanPlay.play();
+		audioPlayingFlg = 1;
+	} else  {
+		audioPlayingID.pause();
+		audioPlayingFlg = 0;
+	}
+	
+	audioPlayingID = oceanPlay;
+	
 }
