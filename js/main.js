768bb43f4263ac28ef0cce42e621f8798058a7e1
Krishnan. N <scratch@srijan.in>
2013-05-28 14:37:15 +0530

    The question click is now a toggle is display / hide the help block.

--- js/main.js
+++ js/main.js
@@ -3,48 +3,47 @@ var hoverOnOcean = document.getElementById("mainpage_ocean");
 var audioPlayingFlg = 0;
 var audioPlayingID;
 
-
 // Hide help by default and display only a circular question mark.
 document.getElementById("help-block").style.visibility = "hidden";
 
 var helpBlock = document.getElementById("question-block");
-helpBlock.onclick = function()  {
-	document.getElementById("help-block").style.visibility = "visible";
+helpBlock.onclick = function() {
+	if (document.getElementById("help-block").style.visibility == "visible")
+		document.getElementById("help-block").style.visibility = "hidden";
+	else
+		document.getElementById("help-block").style.visibility = "visible";
 }
-
-
 // TODO: Pause / play should be a fn, called after simply setting ID.
 
-hoverOnFlag.onclick=function()  {   // TODO: As the name implies, work on hover, not click
+hoverOnFlag.onclick = function() {// TODO: As the name implies, work on hover, not click
 	var whitePlay = document.getElementById("white_play");
-	
+
 	// Bug: If playing a different song and click here, should stop the previous song
 	// and play this song; currently, click is a toggle
-    if (audioPlayingFlg == 0) { 
+	if (audioPlayingFlg == 0) {
 		whitePlay.play();
 		audioPlayingFlg = 1;
-	} else  {
+	} else {
 		audioPlayingID.pause();
 		audioPlayingFlg = 0;
 	}
-			
+
 	audioPlayingID = whitePlay;
 
 }
 
-
-hoverOnOcean.onclick=function()  {   // TODO: As the name implies, work on hover, not click
+hoverOnOcean.onclick = function() {// TODO: As the name implies, work on hover, not click
 	var oceanPlay = document.getElementById("ocean_play");
-	
+
 	// Bug: If playing a different song and click here, should stop the previous song
 	// and play this song; currently, click is a toggle
-    if (audioPlayingFlg == 0) { 
+	if (audioPlayingFlg == 0) {
 		oceanPlay.play();
 		audioPlayingFlg = 1;
-	} else  {
+	} else {
 		audioPlayingID.pause();
 		audioPlayingFlg = 0;
 	}
-	
+
 	audioPlayingID = oceanPlay;
 }
