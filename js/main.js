a1590e97ab2ea54bc15f0d4881bcdd046fd5db9e
Krishnan. N <scratch@srijan.in>
2013-06-12 14:21:46 +0530

    Muse Works at different points. Wooo!

--- js/main.js
+++ js/main.js
@@ -1,62 +1,72 @@
-/* Sound functionality on hover 
+/* Sound functionality on hover
  * Bug: Currently works on mouse click; unsure of design with hover.
  * a) How long should user hover before 'song' changes?
- * b) How to set "theme song" 
+ * b) How to set "theme song"
  */
- 
+
 var hoverOnFlag = document.getElementById('io_main-content');
 var hoverOnOcean = document.getElementById('io_footer-block');
-var audioPlayingFlg = 0;
-var audioPlayingID;
+var g_posx = 0, g_posy = 0;
+var audioPlayingID = 0;
 
-function PlayMuse(x,y)
-{
-	var whitePlay = document.getElementById('white-play');
 
-	// Bug: If playing a different song and click here, should stop the previous song
-	// and play this song; currently, click is a toggle
-	if (audioPlayingFlg == 0) {
-		whitePlay.play();
-		audioPlayingFlg = 1;
-	} else {
-		audioPlayingID.pause();
-		audioPlayingFlg = 0;
-	}
 
-	audioPlayingID = whitePlay;
+hoverOnFlag.addEventListener('click', GetPos, true);
+function GetPos(e) {
+	// BUG. Will not work on IE-8.
+	g_posx = e.clientX;
+	g_posy = e.clientY;
+
+	console.log("position: %d", g_posy);
+}
+
+function GetMuseToPlay() {
+	var play;
+
+	if (g_posy < 150)
+		play = document.getElementById('white-play');
+	else if (g_posy >= 150 && g_posy < 300)
+		play = document.getElementById('green-play');
+	else if (g_posy >= 300)
+		play = document.getElementById('orange-play');
+		
+	return play;
 }
 
 
+function PlayMuse() 
+{
+	if (audioPlayingID != 0)
+		audioPlayingID.pause();
+		
+	audioPlayingID = GetMuseToPlay();
 
-/* 
+	audioPlayingID.play();
+}
+
+/*
 function GetCoordinates(obj)
 {
-    var posxy = document.addEventListener
+var posxy = document.addEventListener
 }
 */
-
-
 
 // TODO: Pause / play should be a fn, called after simply setting ID.
 hoverOnFlag.onclick = function() {// TODO: As the name implies, work on hover, not click
 	// var xy = GetCoordinates();
-	PlayMuse(0,0);
+	PlayMuse();
 }
 
+hoverOnFlag.ondblclick = function() {
+	var anchorElem = document.createElement('a');
+	var imgElem = document.createElement('img');
 
-
-hoverOnFlag.ondblclick = function()  {
-	var anchorElem = document.createElement ('a');
-	var imgElem = document.createElement ('img');
-
-	imgElem.setAttribute ('id', 'aboutus-menu');
-	imgElem.setAttribute ('src', 'img/about_indian_ocean2.jpg');
-	imgElem.setAttribute ('alt', 'About Us');
-	anchorElem.appendChild(imgElem);	
-	hoverOnFlag.appendChild (anchorElem);
+	imgElem.setAttribute('id', 'aboutus-menu');
+	imgElem.setAttribute('src', 'img/about_indian_ocean2.jpg');
+	imgElem.setAttribute('alt', 'About Us');
+	anchorElem.appendChild(imgElem);
+	hoverOnFlag.appendChild(anchorElem);
 }
-
-
 
 hoverOnOcean.onclick = function() {// TODO: As the name implies, work on hover, not click
 	var oceanPlay = document.getElementById('ocean-play');
@@ -73,8 +83,6 @@ hoverOnOcean.onclick = function() {// TODO: As the name implies, work on hover, 
 
 	audioPlayingID = oceanPlay;
 }
-
-
 // Hide help by default and display only a circular question mark.
 var helpBlock = document.getElementById('io_help-block');
 helpBlock.style.visibility = 'hidden';
@@ -86,5 +94,4 @@ queIcon.onclick = function() {
 	else
 		helpBlock.style.visibility = 'visible';
 }
-
 
